7c9c7e719850f385c245421ec9125ebdb3cc57cf
Ayesha <126779551+Ayesha-Pervaiz@users.noreply.github.com>
2023-10-23 17:17:40 +0500

    main

---
+++ Sp21-Bcs-091(Mid_Lab)/Q1.js
@@ -0,0 +1,32 @@
+// script.js
+document.querySelector('.login-button').addEventListener('click', function() {
+    // You can add login functionality here
+    alert('Login button clicked');
+});
+// script.js
+const slider = document.querySelector('.slider-container');
+let slideIndex = 0;
+
+function nextSlide() {
+    if (slideIndex < slider.children.length - 1) {
+        slideIndex++;
+    } else {
+        slideIndex = 0;
+    }
+    updateSlider();
+}
+
+function previousSlide() {
+    if (slideIndex > 0) {
+        slideIndex--;
+    } else {
+        slideIndex = slider.children.length - 1;
+    }
+    updateSlider();
+}
+
+function updateSlider() {
+    slider.style.transform = `translateX(-${slideIndex * 100}%)`;
+}
+
+setInterval(nextSlide, 3000); // Auto slide every 3 seconds
